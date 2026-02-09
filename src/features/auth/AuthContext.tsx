b253fdad8baa7e303458a375635372951e56d954
AbhilashG12 <abhilashggg15@gmail.com>
2026-02-09 21:29:53 +0530

    batman

--- src/features/auth/AuthContext.tsx
+++ src/features/auth/AuthContext.tsx
@@ -1,4 +1,76 @@
 
+// import { useState,useContext,createContext } from "react"
+// import { users } from "../users/users";
+
+// const STORAGE_KEY = 'user';
+
+// interface Auth {
+//     email : string,
+//     fullname : string,
+
+// }
+// interface AuthContext {
+//     user : Auth | null,
+//     login : (email:string,password:string) => boolean,
+//     logout : ()=> void
+// }
+
+// const AuthCntxt = createContext<AuthContext|null>(null);
+
+// export const AuthContext = ({children}:{children:React.ReactNode}) => {
+
+//     const [user,setUser] = useState<Auth|null>(()=>{
+//         const stored = localStorage.getItem(STORAGE_KEY)
+//         return stored ? JSON.parse(stored) : null
+//     })
+
+//     const login =(email:string,password:string):boolean=>{
+//         const found = users.find(user=>user.email==email&&user.password==password)
+//         console.log("user found")
+//         if(!found) return false
+
+//         const authUser : Auth = {
+//             email : found.email,
+//             fullname : found.fullname
+//         }
+        
+//         localStorage.setItem(STORAGE_KEY,JSON.stringify(authUser))
+//         setUser(authUser)
+//         console.log("User logged in sucessfully!")
+//         return true
+//     }
+
+//     const logout =()=>{
+//         localStorage.removeItem(STORAGE_KEY)
+//         setUser(null)
+//     }
+
+//     return(
+//         <AuthCntxt.Provider value={{user,login,logout}}>
+//             {children}
+//         </AuthCntxt.Provider>
+//     )
+
+// }
+
+
+// // eslint-disable-next-line react-refresh/only-export-components
+// export const useAuth =()=>{
+//     const ctx = useContext(AuthCntxt);
+
+//     if(!ctx){
+//         throw new Error("useAuth must be used inside AuthProvider")
+//     }
+//     return ctx
+// }
+
+// export default AuthContext
+
+
+
+
+
+
 import { useState,useContext,createContext } from "react"
 import { users } from "../users/users";
 
--- src/features/auth/Protect.tsx
+++ src/features/auth/Protect.tsx
@@ -1,15 +1,68 @@
+// import { useAuth } from "./AuthContext"
+// import { Navigate } from "react-router-dom"
+// const Protect = ({children}:{children:React.ReactNode}) => {
+//     const {user} = useAuth()
+//     if(!user){
+//         return <Navigate to="/login" replace/>
+//     }
+//   return (
+//     <>
+//       {children}
+//     </>
+//   )
+// }
+
+// export default Protect
+
+
+import {motion} from "framer-motion"
+import {FaEye,FaEyeSlash} from "react-icons/fa"
+import {  useState } from "react"
+import {Link,useNavigate} from "react-router-dom"
+import {users} from "../users/users"
 import { useAuth } from "./AuthContext"
-import { Navigate } from "react-router-dom"
-const Protect = ({children}:{children:React.ReactNode}) => {
-    const {user} = useAuth()
-    if(!user){
-        return <Navigate to="/login" replace/>
+
+
+const Login = () => {
+  const navigate = useNavigate()
+  const {login} = useAuth()
+  const [show,setShow] = useState<boolean>(true);
+  const [mail,setMail] = useState("");
+  const [pass,setPass] = useState("")
+
+  const handleLogin =(mail:string,pass:string)=>{
+    const user = users.find(user=>mail==user.email && user.password==pass)
+    if(user){
+      login(mail,pass)
+      navigate("/dashboard")
+    }else{
+      alert("Cannot Find User")
     }
+      
+  }
+
   return (
-    <>
-      {children}
-    </>
+    <div className="flex justify-center mt-30">
+      <motion.div className="border flex flex-col w-110 h-120 rounded-xl shadow-xl bg-gray-50" initial={{ opacity: 0, x: 100 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: -100 }}
+        transition={{ duration: 0.5 }}
+      >
+            <h1 className="text-3xl font-bold font-serif ml-40 mt-5">Login</h1>
+                <div className="mt-5 flex flex-col gap-10">
+                    <input value={mail} type="email"  className="border w-90 ml-10 rounded h-10 shadow pl-2 bg-white" placeholder="Enter your email" required onChange={(e)=>{setMail(e.target.value)}} />
+                    <div className="relative w-90 ml-10">
+                    <input value={pass} type={show? "password" : "text"} className="border w-full rounded h-10 shadow pl-2 bg-white" placeholder="Enter your password" required onChange={e=>{setPass(e.target.value)}}/>
+                    <button  onClick={()=>{setShow(!show)}} className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-black cursor-pointer">{show?<FaEye/>:<FaEyeSlash/>}</button>
+                    </div>
+                    <button type="submit" onClick={()=>{handleLogin(mail,pass)}}  className="border w-70 ml-20 rounded-2xl h-10 shadow bg-white font-mono cursor-pointer hover:bg-blue-300">Submit</button>
+                </div>
+                <div className="ml-20 mt-10">
+                  <p>Dont have an account? <Link className="underline text-black font-bold" to="/">Signup</Link> instead</p>
+                </div>
+        </motion.div>
+    </div>
   )
 }
 
-export default Protect
+export default Login
