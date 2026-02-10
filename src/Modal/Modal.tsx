9f1651368ba19bc6c8232015ab467f9c0aa265f4
AbhilashG12 <abhilashggg15@gmail.com>
2026-02-10 21:54:21 +0530

    final

--- src/Modal/Modal.tsx
+++ src/Modal/Modal.tsx
@@ -1,80 +1,82 @@
-import { useState,useContext,createContext } from "react"
+// import { useState,useContext,createContext } from "react"
 
-interface ModalType {
-    isOpen : boolean,
-    open : () => void,
-    close : () => void,
-}
+// interface ModalType {
+//     isOpen : boolean,
+//     open : () => void,
+//     close : () => void,
+// }
 
-const ModalContext = createContext<ModalType|null>(null)
+// const ModalContext = createContext<ModalType|null>(null)
 
-const Modal = ({children}:{children:React.ReactNode}) => {
-    const [isOpen,setIsopen] = useState(false);
-    const open=()=> setIsopen(true);
-    const close=()=> setIsopen(false)
-  return (
-    <ModalContext.Provider value={{isOpen,open,close}}>
-      {children}
-    </ModalContext.Provider>
-  )
-}
-
-
- const useModal=()=>{
-    const ctx = useContext(ModalContext);
-    if(!ctx){
-        throw new Error("Modal components should be inside Provider")
-    }
-    return ctx
-}
+// const Modal = ({children}:{children:React.ReactNode}) => {
+//     const [isOpen,setIsopen] = useState(false);
+//     const open=()=> setIsopen(true);
+//     const close=()=> setIsopen(false)
+//   return (
+//     <ModalContext.Provider value={{isOpen,open,close}}>
+//       {children}
+//     </ModalContext.Provider>
+//   )
+// }
 
 
-const Trigger=({children}:{children:React.ReactNode})=>{
-    const {open} = useModal();
-
-    return (
-        <div onClick={open} className="inline-block">
-            {children}
-        </div>
-    )
-
-}
-
-const Content =({children}:{children:React.ReactNode})=>{
-
-    const {isOpen,close} = useModal();
-
-    if(!isOpen) return null;
-
-    return (
-        <div className="fixed inset-0 bg-black/40 flex items-center justify-center" onClick={close}>
-            <div className="bg-white rounded-lg p-6 min-w-[300px]" onClick={(e) => e.stopPropagation()}>
-                {children}
-            </div>
-        </div>
-    )
-
-}
-
-const Header = ({ children }: { children: React.ReactNode }) => {
-  const { close } = useModal()
-
-  return (
-    <div className="flex justify-between items-center mb-4">
-      <h2 className="text-xl font-bold">{children}</h2>
-      <button onClick={close}>✕</button>
-    </div>
-  )
-}
-
-const Body = ({ children }: { children: React.ReactNode }) => {
-  return <div className="text-gray-700">{children}</div>
-}
-
-Modal.Trigger = Trigger
-Modal.Content = Content
-Modal.Header = Header
-Modal.Body = Body
+//  const useModal=()=>{
+//     const ctx = useContext(ModalContext);
+//     if(!ctx){
+//         throw new Error("Modal components should be inside Provider")
+//     }
+//     return ctx
+// }
 
 
-export default Modal
+// const Trigger=({children}:{children:React.ReactNode})=>{
+//     const {open} = useModal();
+
+//     return (
+//         <div onClick={open} className="inline-block">
+//             {children}
+//         </div>
+//     )
+
+// }
+
+// const Content =({children}:{children:React.ReactNode})=>{
+
+//     const {isOpen,close} = useModal();
+
+//     if(!isOpen) return null;
+
+//     return (
+//         <div className="fixed inset-0 bg-black/40 flex items-center justify-center" onClick={close}>
+//             <div className="bg-white rounded-lg p-6 min-w-[300px]" onClick={(e) => e.stopPropagation()}>
+//                 {children}
+//             </div>
+//         </div>
+//     )
+
+// }
+
+// const Header = ({ children }: { children: React.ReactNode }) => {
+//   const { close } = useModal()
+
+//   return (
+//     <div className="flex justify-between items-center mb-4">
+//       <h2 className="text-xl font-bold">{children}</h2>
+//       <button onClick={close}>✕</button>
+//     </div>
+//   )
+// }
+
+// const Body = ({ children }: { children: React.ReactNode }) => {
+//   return <div className="text-gray-700">{children}</div>
+// }
+
+// Modal.Trigger = Trigger
+// Modal.Content = Content
+// Modal.Header = Header
+// Modal.Body = Body
+
+
+// export default Modal
+
+
--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -1,3 +1,49 @@
+// import { useMemo,useState } from "react"
+// import { users , type Users } from "../features/users/users"
+
+// type Order = "asc" | "desc";
+// type SortKey = "name" | "role"
+// export const useUsers = () => {
+//     const [search,setSearch] = useState("")
+//     const [sortKey,setSort] = useState("name")
+//     const [order,setOrder] = useState<Order>("asc")
+
+//     const filtred = useMemo(()=>{
+//         const result = users.filter((user)=>`${user.fullname} ${user.email}`.toLowerCase().includes(search.toLowerCase()))
+//         result.sort((a,b)=>{
+//             const valA = a[sortKey as keyof Users]
+//             const valB = b[sortKey as keyof Users]
+
+//              if (valA < valB) return order === "asc" ? -1 : 1
+//              if (valA > valB) return order === "asc" ? 1 : -1
+//              return 0
+//         })
+//     },[search,sortKey,order])
+
+
+//     const toggleSort=(key:SortKey)=>{
+//         if(sortKey == key){
+//             setOrder((prev)=>(prev === "asc" ? "desc" : "asc"))
+//         }else{
+//             setSort(key)
+//             setOrder("asc")
+//         }
+//     }
+
+
+//     return {
+//         users , filtred,
+//         search,
+//         setSearch,
+//         sortKey,
+//         order,
+//         toggleSort ,
+        
+//     }
+    
+// }
+
+
 import { useMemo,useState } from "react"
 import { users , type Users } from "../features/users/users"
 
